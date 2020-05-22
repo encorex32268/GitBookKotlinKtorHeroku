80a6aa9b2d60b90784d35ba9a1b25ccb8433a1c3
LeeChen <encorex32268@gmail.com>
2020-05-22 18:05:37 +0900

    Initial commit & Part1

---
+++ src/main/kotlin/MainKtor.kt
@@ -0,0 +1,38 @@
+import io.ktor.application.call
+import io.ktor.html.respondHtml
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.route
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.head
+import kotlinx.html.title
+
+fun main() {
+    val server = embeddedServer(Netty,port = 7070){
+        routing {
+            route("/"){
+                get{
+                    call.respondHtml {
+                        head {
+                            title {
+                                text("Ktor web")
+                            }
+                        }
+                        body {
+                            h1 {
+                                text("Hello Ktor")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    server.start(wait = true)
+
+
+}
